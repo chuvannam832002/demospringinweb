0dfce80b6f11c241735943bb5148c6966dcfbb95
Chu Van Nam <chuvannam70@gmail.com>
2022-12-17 07:52:41 +0700

    Practice with auto run and actuator

--- src/main/java/com/nbstech/spring/basic/Movierecommender/MovierecommenderApplication.java
+++ src/main/java/com/nbstech/spring/basic/Movierecommender/MovierecommenderApplication.java
@@ -2,6 +2,7 @@ package com.nbstech.spring.basic.Movierecommender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MovierecommenderApplication {
@@ -9,5 +10,6 @@ public class MovierecommenderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovierecommenderApplication.class, args);
 	}
+
 
 }
